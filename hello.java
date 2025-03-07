9f90e7ab18c4e3d0c44782733372524f3767695c
rishitha27-06 <rishithaachanta27@gmail.com>
2025-03-07 09:14:47 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Print Hello, World! to the console
         System.out.println("Hello, World!");
+         // IT IS PRINTED
     }
 }
